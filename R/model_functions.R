a017c58a52b603e734ef12aa992190fbce582d73
Matt Espe <mespe@ucdavis.edu>
2018-07-25 13:43:35 -0600

    Fixes to functions, simple sim script.

--- R/model_functions.R
+++ R/model_functions.R
@@ -4,7 +4,7 @@
 # From Schelegle et al.
 
 UOS = function(O3, Ve, t, # inputs
-               DR = O3 * Ve * 1.196,
+               DR = O3 * Ve * 1.96,
                CD = cuml_integral(DR, t),
                Dos) #parameter
     # Vectorized means for calc UOS over a number of t timepoints
@@ -12,23 +12,23 @@ UOS = function(O3, Ve, t, # inputs
     DR / (1 + exp(-20 * (t - (Dos * t / CD))))
 }
 
-FEV = function(UOS, n_t = length(UOS), #input
+FEV = function(UOS, n_t = length(UOS), fev_base, #input
                K, A # parameters
                )
     # Takes vectors of UOS and calculates FEV
 {
-    r = (1 - exp(-k))
+    r = (1 - exp(-K))
     
     x = numeric(n_t)
 
-    # Not sure if this is the correct way to do this
-    x[1] = 0
+    # Not right way to do this - every person starts with a baseline
+    x[1] = fev_base
     
     for(i in 2:n_t){
-        x[i] = ((UOS[i - 1] / K) - x[i - 1]) * r
+        x[i] = x[i-1] + (((UOS[i - 1] / K) - x[i - 1]) * r)
     }
     
-    sum(x) * A
+    cumsum(x) * A
 }
 
 cuml_integral = function(x, t)
--- scripts/simulate_data.R
+++ scripts/simulate_data.R
@@ -1,2 +1,18 @@
 # Functions for simulating data according to each model
 
+source("R/model_functions.R")
+# source("R/sim_funs.R")
+
+# Using z4901_HR7PRED.xls as an example
+
+time = c(55, 115, 180, 270, 330, 395)
+
+o3 = c(0, 0.08, 0.1)
+
+ve = rnorm(n = length(time), mean = 40, sd = 5)
+
+dos = 1050 # mean of m/f from paper
+
+uos = UOS(o3[2], ve, time, Dos = dos)
+
+FEV(UOS = uos, K = 0.2, A = -0.02, fev_base = 4.6)
