93e0aa867726fcc6befd24f6fe4c19a927eee29c
Matt Espe <mespe@ucdavis.edu>
2018-12-05 15:48:02 -0700

    Update test to source in.

--- tests/test_cuml_integral.R
+++ tests/test_cuml_integral.R
@@ -2,13 +2,13 @@ x = c(1, 1, 1)
 t = c(1, 3, 5)
 
 #expect .5, 2.5, 4.5
-cuml_integral(x, t)
+stopifnot(identical(cuml_integral(x, t), c(.5,2.5, 4.5)))
 
 #expect 1, 3, 5
-cuml_integral(c(1, x), c(0, t))
+stopifnot(identical(cuml_integral(c(1, x), c(0, t)), c(0,1, 3, 5)))
 
 # expect 0 0 0
-cuml_integral(c(0,0,0), 1:3)
+stopifnot(identical(cuml_integral(c(0,0,0), 1:3), rep(0,3)))
 
 # triangle - expect last value == 3*3/2
-cuml_integral(0:3, 0:3)
+stopifnot(cuml_integral(0:3, 0:3)[4] == 3*3/2) 
