70d64b9a7aa87af2d5805bc0903d452444c99aa6
Duncan Temple Lang <duncan@r-project.org>
2018-12-21 20:05:36 -0800

    code to run the grid search over the three parameters. Takes about 3 mins on dsi machine for 27000 combinations of values with 30 cores.

---
+++ scripts/gridSearch.R
@@ -0,0 +1,24 @@
+# Fit the data from a single sheet
+source("R/fit_funs.R")
+source("R/model_functions.R")
+source("R/readUCD.R")
+f = "data/UCD_WCA2002.xls"
+
+d = readExp(f)
+d = lapply(d, deltaFEV1)
+
+
+numIntervals = 30
+dos = seq(5, 2500, length.out = numIntervals)
+K = seq(log(2)/1000, log(2)/1, length.out = numIntervals)
+A = seq(-0.15, 0, length.out = numIntervals) 
+#pars = c(0.69, -0.0135)
+pars = expand.grid(DOS = dos, K = K, A = A)
+
+gr = apply(pars, 1, function(x) fit_FEV1b(unlist(x), d = d))
+
+library(parallel)
+cl = makeCluster(30, "FORK")
+grp = parallel::parApply(cl, pars, 1, function(x) fit_FEV1b(unlist(x), d = d))
+
+
