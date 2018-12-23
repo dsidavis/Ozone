af3be9a04c0eb0425c5c13cfc77ff73c7aa9b1d3
Duncan Temple Lang <duncan@r-project.org>
2018-12-22 16:28:59 -0800

    code to perform the grid search over the three parameters for the minumum sum of squares.

--- scripts/gridSearch.R
+++ scripts/gridSearch.R
@@ -8,17 +8,19 @@ d = readExp(f)
 d = lapply(d, deltaFEV1)
 
 
-numIntervals = 30
+numIntervals = 50
 dos = seq(5, 2500, length.out = numIntervals)
+dos = exp(seq(1,9, length.out = numIntervals))
 K = seq(log(2)/1000, log(2)/1, length.out = numIntervals)
-A = seq(-0.15, 0, length.out = numIntervals) 
+A = seq(-0.15, 0, length.out = numIntervals)
+#A = seq(0, 1, length.out = numIntervals) 
 #pars = c(0.69, -0.0135)
 pars = expand.grid(DOS = dos, K = K, A = A)
 
 gr = apply(pars, 1, function(x) fit_FEV1b(unlist(x), d = d))
 
 library(parallel)
-cl = makeCluster(30, "FORK")
-grp = parallel::parApply(cl, pars, 1, function(x) fit_FEV1b(unlist(x), d = d))
+cl = makeCluster(60, "FORK")
+grp = parallel::parApply(cl, pars, 1, function(x) fit_FEV1b(pars = unlist(x), d = d))
 
 
