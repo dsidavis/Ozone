1bbd942260437e52b89b57276f279e35cad4c497
Matt Espe <mespe@ucdavis.edu>
2019-01-10 12:00:58 -0700

    Update with load data.

--- scripts/fit_SAS.R
+++ scripts/fit_SAS.R
@@ -1,10 +1,13 @@
-# which have a >0 dFEV1
 source("R/model_functions.R")
 source("R/fit_funs.R")
 
-i = sapply(sas[[3]], function(x) any(x$dFEV1 > 0, na.rm = TRUE))
+sas = readRDS("Ozone_data_from_SAS.rds")
+if(FALSE)
+    # Which have dFEV other than 0
+i = sapply(sas[[3]], function(x) any(x$dFEV1 != 0, na.rm = TRUE))
 sas[[1]][i]
 fit_FEV1b(c(1100, 0.69, -.02), sas[[1]])
+}
 
 # Try the optimization
 fit = optim(par = c(Dos = 900, K = 0.65, A = -0.020), fn = fit_FEV1b,
