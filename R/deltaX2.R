c34f744a9414aa0b4cd8f02a5d6154f00b75b14e
Duncan Temple Lang <duncan@r-project.org>
2019-01-26 14:11:49 -0800

    initial version of C version of deltaX loop

---
+++ R/deltaX2.R
@@ -0,0 +1,11 @@
+deltaX = 
+function(UOS, n_t = length(UOS), fev_base, #input
+               K # parameters
+               )
+    # Takes vectors of UOS and calculates FEV
+{
+    r = (1 - exp(-K))
+    ceoss = UOS / K
+
+    .Call("R_deltaXLoop", r, ceoss, fev_base)
+}    
---
+++ src/deltaX.c
@@ -0,0 +1,30 @@
+#include <Rdefines.h>
+
+
+/* 
+  fev_base  numeric(1) - scalar real
+  ceoss  numeric(n_t)
+  r_r   scalar containing named value for K
+ */
+SEXP
+R_deltaXLoop(SEXP r_r, SEXP r_ceoss,  SEXP fev_base)
+{
+    int i, n_t = Rf_length(r_ceoss);
+    SEXP r_x;
+    PROTECT(r_x = NEW_NUMERIC(n_t));
+    double *x = REAL(r_x);
+    double *ceoss = REAL(r_ceoss);
+    x[0] = REAL(fev_base)[0];
+    double r = REAL(r_r)[0];
+
+/*
+        for(i in 2:n_t)
+            # sum as we go
+            x[i] = x[i-1] + ((ceoss[i - 1] - x[i - 1]) * r)    
+*/
+    for(i = 1; i < n_t; i++) {
+	x[i] = x[i-1] + ((ceoss[i - 1] - x[i - 1]) * r);
+    }
+    UNPROTECT(1);
+    return(r_x);
+}
