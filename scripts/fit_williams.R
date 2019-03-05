cf5e271ec89624726e1dfe5efc905b1431744fa4
Matt Espe <mespe@ucdavis.edu>
2019-03-05 16:45:57 -0700

    Look across a wider range, some EDA on results.

--- scripts/fit_williams.R
+++ scripts/fit_williams.R
@@ -7,25 +7,29 @@ mod = stan_model("src/williams.stan")
 
 source("scripts/munge_data_williams.R")
 
-
+# List from original code
 init_list = list(B = c(1.475, -0.4847, 0.0068, 49.2633, 0.0042, 1.0, 0, 0.0001, 10),
-                 U = rep(0.01, 540)) 
+                 U = rep(0.01, 540))
+
+# Based on optim
+init_list = list(B = c(5.24510075177552, -0.215188297213872, 2.23839744913426, 
+                       20.9084495320374, -1.26311776378368, 1.36166512931033, 
+                       0, -0.128347714260874, 13.9865428397586),
+                 U = rep(0.01, 540))
+
 ans$sigma_U = 1
 # b = replicate(100, system.time({fit = optimizing(mod, data = ans, init = init_list)}))
 fit = optimizing(mod, data = ans, init = init_list, verbose = TRUE)
 
-fit = sampling(mod, data = ans, cores = 4)
-
-
-
-b = replicate(50, optimizing(mod, data = ans,
+b = replicate(100, try({optimizing(mod, data = ans,
                              init = sapply(init_list, function(x)
-                                 rnorm(length(x), x, abs(x/10)),
-                                 simplify = FALSE))$par[1:9])
-summary(t(b))
-b = replicate(100, optimizing(mod, data = ans)$value)
+                                 rnorm(length(x), x, abs(x)),
+                                 simplify = FALSE))}))
 
-fit = optimizing(mod, data = ans, algorithm = "Fixed_param", init = init_list)
+ww = do.call(rbind, b["par",])[,1:9]
+ww = cbind(ww, ll = unlist(b["value",]))
+i = ww[,"ll"] > -7500
+summary(ww[i,])
+pairs(ww[i,])
+ww[which.max(ww[,"ll"]),]
 
-
-mod2 = 
